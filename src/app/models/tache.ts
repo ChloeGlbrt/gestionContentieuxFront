22df22e2e03014d4e69f41a2a7996fade894533e
Sandra-Latournerie <s.latournerie@live.fr>
2023-03-10 09:51:32 +0100

    Sandra2

--- src/app/models/tache.ts
+++ src/app/models/tache.ts
@@ -1,2 +1,19 @@
+import { Affaire } from "./affaire";
+import { Phase } from "./phase";
+import { Tribunal } from "./tribunal";
+import { Utilisateur } from "./utilisateur";
+
 export class Tache {
+    
+    idTache!: number;
+    dateCreation!: Date;
+    titre!: string;
+    description!: string;
+    statutAudience!: boolean;
+    utilisateur!: Utilisateur;
+    affaire!: Affaire;
+    tribunal!: Tribunal;
+    phase!: Phase;
+
+
 }
--- src/app/services/tache.service.ts
+++ src/app/services/tache.service.ts
@@ -5,5 +5,23 @@ import { Injectable } from '@angular/core';
 })
 export class TacheService {
 
-  constructor() { }
+  private BASE_URL = "http://localhost:9090/taches";
+
+  constructor(private httpClient: HttpClient) { }
+
+  
+  public findAll(): Observable<any> {
+    return this.httpClient.get(this.BASE_URL);
+    // Afficher la lise des users
+  }
+  // save --> verbe http : POST --> url : BASE_URL + Body
+  public save(user: any): Observable<any> {
+    console.log('Save service');
+    return this.httpClient.post(this.BASE_URL, user);
+  }
+  // delete --> verbe http : DELETE --> url : BASE_URL/id
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete(this.BASE_URL + "/" + id);
+  }
+
 }
